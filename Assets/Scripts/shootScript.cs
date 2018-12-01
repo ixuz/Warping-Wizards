88ca8aeed22a5eae83e0ea5c073c3413689c21db
kobebbobab <tage.akerstrom@gmail.com>
2018-12-01 22:12:23 +0100

    added flare to fireball

--- Assets/Scripts/shootScript.cs
+++ Assets/Scripts/shootScript.cs
@@ -9,14 +9,23 @@ public class shootScript : MonoBehaviour {
     public GameObject Fireball;
     Vector2 direction;
 
+    public float CoolDown;
+    public float CoolDownTime;
+
+    private void Start()
+    {
+        CoolDown = CoolDownTime;
+    }
+
     void Update()
     {
 
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && CoolDown < 0)
         {
             fire();
         }
+        CoolDown = CoolDown - Time.deltaTime;
 
     }
 
@@ -32,5 +41,6 @@ public class shootScript : MonoBehaviour {
         direction = mousePos - Aim.position;
         direction.Normalize();
         Instantiate(Fireball, Aim.position, Quaternion.LookRotation(direction));
+        CoolDown = CoolDownTime;
     }
 }
