1861aee997dd628683625ff133775f2d43c1a18f
kobebbobab <tage.akerstrom@gmail.com>
2018-12-01 19:18:43 +0100

    shooting fireballs

---
+++ Assets/FireballScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour {
+
+    public GameObject ThisObject;
+    public float Speed = 2f;
+    public Transform ExplodePoint;
+    public Transform SpawnPoint;
+
+    private void Start()
+    {
+        Destroy(ThisObject.gameObject, 5);
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log(coll.gameObject.transform.name);
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+            Debug.Log("hit 1");
+            Instantiate(ExplodePoint, SpawnPoint.position, SpawnPoint.rotation);
+                Destroy(ThisObject, 0);
+
+    }
+}
