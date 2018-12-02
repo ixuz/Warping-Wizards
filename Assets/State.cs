22c2780d0f8e309613fc5aab35d3b2cbe2987374
Anton Wiklund <ixuz07@gmail.com>
2018-12-02 16:16:59 +0100

    Added difficultly levels

---
+++ Assets/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour {
+
+  public static State instance;
+
+  public int difficultyLevel = 1; // 1, 2 or 3
+
+  void Awake() {
+
+    if (instance == null) {
+      instance = this;
+    } else {
+      Destroy(gameObject);
+      return;
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+  public void SetDifficultyLevel(int difficultyLevel) {
+    this.difficultyLevel = difficultyLevel;
+  }
+}
