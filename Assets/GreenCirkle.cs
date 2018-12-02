49e119130966b19773852343935c6e377f4f4ec0
KobebBobab <37380717+KobebBobab@users.noreply.github.com>
2018-12-02 20:24:07 +0100

    added end trigger

---
+++ Assets/GreenCirkle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenCirkle : MonoBehaviour {
+
+    public GameObject BloodExplosion;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            AudioManager.instance.PlaySfx("StartMobSpawners");
+            Instantiate(BloodExplosion, transform.position, transform.rotation);
+        }
+    }
+
+}
