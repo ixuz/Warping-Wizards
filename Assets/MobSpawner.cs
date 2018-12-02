a92bbb53e3dbee47ab9d8cfb1cf23fad73ee2497
Anton Wiklund <ixuz07@gmail.com>
2018-12-02 13:12:06 +0100

    Mob spawner

---
+++ Assets/MobSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour {
+
+  public GameObject[] mobPrefabs;
+  public float spawnRate = 1.0f;
+
+  float cooldown = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+    cooldown = 1.0f / spawnRate;
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    cooldown -= Time.deltaTime;
+    if (cooldown <= 0) {
+      cooldown = 1.0f/spawnRate;
+      SpawnRandomMob();
+    }
+	}
+
+  void SpawnRandomMob() {
+    GameObject mobPrefab = mobPrefabs[Random.Range(0, mobPrefabs.Length)];
+    Instantiate(mobPrefab, transform.position, Quaternion.identity);
+  }
+}
