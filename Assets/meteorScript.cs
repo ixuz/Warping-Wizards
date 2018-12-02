ca182891443f98bfe84ab242630a83e2b6bf78df
kobebbobab <tage.akerstrom@gmail.com>
2018-12-02 12:19:31 +0100

    made a meteor

---
+++ Assets/meteorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorScript : MonoBehaviour {
+
+    public GameObject thisObject;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(thisObject, 3.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
