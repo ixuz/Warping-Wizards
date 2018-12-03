4f31e8bc68fb1ec1420426959d641c7fba747a3e
Anton Wiklund <ixuz07@gmail.com>
2018-12-04 00:19:44 +0100

    LDJam publish

---
+++ Assets/MouseCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+  public Texture2D cursor;
+  public bool lockCursor = false;
+
+	void Start () {
+
+  }
+	
+	// Update is called once per frame
+	void Update () {
+
+  }
+}
