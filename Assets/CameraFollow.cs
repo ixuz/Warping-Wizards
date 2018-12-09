9641509b3fe3f033914a09391da85b99e71a2d31
Frenchie14 <mbize2005@gmail.com>
2018-12-08 23:30:02 -0800

    Re-implement blink to go in the users's direction

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-  public Transform followObject;
+  public GameObject followObject;
 
 	// Use this for initialization
 	void Start () {
